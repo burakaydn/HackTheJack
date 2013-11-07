cfae9876ba11003d9e621a9e9adb5b0e63e78a67
burakaydn <ydnburak@gmail.com>
2013-11-07 10:46:03 +0200

    initial commit

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Hack the Jack',
+    // subtitle: 'Subtitle Goes Here',
+    eventInfo: {
+     title: 'DevFest Eskişehir',
+     date: '9/11/2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Burak Aydın',
+    company: 'Globsis, Ankara Üni. BÖTE 4',
+    gplus: 'https://plus.google.com/u/0/110957080102445790184/',
+    twitter: '@burakaydn',
+    www: 'http://burakaydin.net.tr',
+    github: 'http://github.com/burakaydn'
+  }, {
+    name: 'Efecan Kocuklu',
+    company: 'ESOGÜ ..... 1<br>',
+    gplus: 'https://plus.google.com/u/0/115902911550197573440/posts',
+    twitter: '@efecankocuklu',
+    www: 'http://www.efecankocuklu.com.tr',
+    github: 'http://github.com/efecankocuklu'
+  }]
+};
+
